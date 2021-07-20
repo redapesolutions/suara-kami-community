0715f328573191c7a3a5b13aa2557f9e5a15a1a5
sani <khursani8@gmail.com>
2021-07-20 09:14:19 +0000

    remove pytorch dependency
    Signed-off-by: sani <khursani8@gmail.com>

--- sk/sk.py
+++ sk/sk.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 import onnxruntime
-import torch
 from pqdm.threads import pqdm
 from pathlib import Path
 import numpy as np
@@ -67,14 +66,17 @@ def get_files(path, extensions=None, recurse=True, folders=None, followlinks=Tru
 try:
     import onnxruntime
     def to_numpy(tensor):
-        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+        if "requires_grad" in tensor:
+            return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+        else:
+            return tensor
     @patch
     def __call__(self:onnxruntime.capi.onnxruntime_inference_collection.InferenceSession, xs, length=None):
         ort_inputs = {self.get_inputs()[0].name: to_numpy(xs)}
         ologits = self.run(None, ort_inputs)
         alogits = np.asarray(ologits[0])
-        logits = torch.from_numpy(alogits[0])
-        return logits,int(ologits[1][0])
+        logits = alogits[0]
+        return logits
 
 except Exception as e:
     print(e)
@@ -89,10 +91,7 @@ def decode(out):
     return "".join([labels[i] for i in collapsed])
 
 def decodes(output):
-    if breakpoint:
-        n_jobs = 1
-    else:
-        n_jobs = 4
+    n_jobs = 4
     return pqdm(output,decode,n_jobs=n_jobs,disable=True)
 
 def inference(model,xs):
@@ -100,12 +99,12 @@ def inference(model,xs):
         import tensorflow as tf
         xs = tf.constant(xs)
         output = model(xs)["output_0"].numpy()
-        output = torch.as_tensor(output)[0]
     else:
-        output,_ = model(xs)
-    log_probs = torch.nn.functional.log_softmax(output,-1) # for entropy later
+        output = model(xs)
+    log_probs = output
+    # log_probs = torch.nn.functional.log_softmax(output,-1) # for entropy later
     entropy = -(log_probs.exp() * log_probs).sum(dim=-1).mean(-1)
-    log_probs = log_probs.argmax(-1).type(torch.int)
+    log_probs = log_probs.argmax(-1)
     log_probs = log_probs.contiguous()
     text = decode(log_probs)
     timesteps = [0]
@@ -115,17 +114,17 @@ def inference_lm(model,xs,decoder):
     if "tensorflow" in str(type(model)):
         import tensorflow as tf
         xs = tf.constant(xs)
-        output = model(xs)["output_0"].numpy()
-        output = torch.as_tensor(output)[0]
+        output = model(xs)["output_0"].numpy()[0]
+        # output = torch.as_tensor(output)[0]
     else:
-        output,_ = model(xs)
+        output = model(xs)
     if output.ndim == 3:
         output = output[0]
-    probs = torch.nn.functional.softmax(output,-1)
-    out = decoder.decode_beams(to_numpy(probs),prune_history=True)
+    log_probs = output
+    # probs = torch.nn.functional.softmax(output,-1)
+    out = decoder.decode_beams(to_numpy(log_probs),prune_history=True)
     text, lm_state, timesteps, logit_score, lm_score = out[0]
-    log_probs = probs.log()
-    entropy = -(log_probs.exp() * log_probs).sum(dim=-1)
+    entropy = -(np.exp(log_probs) * log_probs).sum(-1)
     time = [i[-1] for i in timesteps]
     entropy = [entropy[i[0]:i[1]].sum().item() for i in time]
     return text,entropy,timesteps
@@ -164,6 +163,7 @@ def load_model(path):
         sess_options.intra_op_num_threads = multiprocessing.cpu_count()
         model = onnxruntime.InferenceSession(str(path),sess_options)
     elif path.suffix == ".pth":
+        import torch
         model = torch.load(str(path))
         model = model.eval()
     else:
@@ -228,7 +228,7 @@ def predict(fn,model=None,decoder=None,output_folder=None,output_csv=None):
         except Exception as e:
             print(f"failed,",e)
             continue
-        xs = torch.as_tensor(data)[None]
+        xs = data[None]
         if decoder:
             text,entropy,tt = inference_lm(model,xs,decoder)
         else:
