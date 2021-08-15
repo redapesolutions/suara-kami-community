d6a66538c5a3620ece6d8f969c7921b91da8f102
sani <khursani8@gmail.com>
2021-08-15 19:18:39 +0000

    grpc

--- server/grpc/stt_server.py
+++ server/grpc/stt_server.py
@@ -1,20 +1,4 @@
 #!/usr/bin/env python3
-#
-# Copyright 2020 Alpha Cephei Inc
-# Copyright 2015 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""The Python implementation of the gRPC route guide server."""
 
 from concurrent import futures
 import os
@@ -28,27 +12,16 @@ import grpc
 import stt_service_pb2
 import stt_service_pb2_grpc
 from google.protobuf import duration_pb2
+import numpy as np
 
+# sys.path.append(".")
+from sk import predict
 
-
-# Uncomment for better memory usage
-# import gc
-# gc.set_threshold(0)
-
-vosk_interface = os.environ.get('VOSK_SERVER_INTERFACE', '0.0.0.0')
-vosk_port = int(os.environ.get('VOSK_SERVER_PORT', 5001))
-vosk_model_path = os.environ.get('VOSK_MODEL_PATH', 'model')
-vosk_sample_rate = float(os.environ.get('VOSK_SAMPLE_RATE', 8000))
-
-if len(sys.argv) > 1:
-   vosk_model_path = sys.argv[1]
+import gc
+gc.set_threshold(0)
 
 class SttServiceServicer(stt_service_pb2_grpc.SttServiceServicer):
     """Provides methods that implement functionality of route guide server."""
-
-    def __init__(self):
-        # self.model = Model(vosk_model_path)
-        self.model = 
 
     def get_duration(self, x):
         seconds = int(x)
@@ -61,7 +34,10 @@ class SttServiceServicer(stt_service_pb2_grpc.SttServiceServicer):
                                         word=x['word'], confidence=x['conf'])
 
     def get_response(self, json_res):
-        res = json.loads(json_res)
+        try:
+            res = json.loads(json_res)
+        except Exception as e:
+            print(e)
         if 'partial' in res:
              alternatives = [stt_service_pb2.SpeechRecognitionAlternative(text=res['partial'])]
              chunks = [stt_service_pb2.SpeechRecognitionChunk(alternatives=alternatives, final=False)]
@@ -69,7 +45,10 @@ class SttServiceServicer(stt_service_pb2_grpc.SttServiceServicer):
         else:
              words = [self.get_word_info(x) for x in res.get('result', [])]
              confs = [w.confidence for w in words]
-             alt_conf = sum(confs) / len(confs)
+             if len(confs) == 0:
+                 alt_conf = 0
+             else:
+                 alt_conf = sum(confs) / len(confs)
              alternatives = [stt_service_pb2.SpeechRecognitionAlternative(text=res['text'], words=words, confidence=alt_conf)]
              chunks = [stt_service_pb2.SpeechRecognitionChunk(alternatives=alternatives, final=True)]
              return stt_service_pb2.StreamingRecognitionResponse(chunks=chunks)
@@ -77,23 +56,38 @@ class SttServiceServicer(stt_service_pb2_grpc.SttServiceServicer):
     def StreamingRecognize(self, request_iterator, context):
         request = next(request_iterator)
         partial = request.config.specification.partial_results
-        recognizer = KaldiRecognizer(self.model, request.config.specification.sample_rate_hertz)
+        buffer = []
+        recent_text = ""
+        repeated = 0
+        recent_response = ""
+        final_text = ""
         for request in request_iterator:
-            res = recognizer.AcceptWaveform(request.audio_content)
-            if res:
-                yield self.get_response(recognizer.Result())
-            elif partial:
-                yield self.get_response(recognizer.PartialResult())
-        yield self.get_response(recognizer.FinalResult())
+            buffer.append(request.audio_content)
+            text,_,_,_ = predict(np.array(buffer).flatten().tobytes())
+            if text:
+                if recent_text == text:
+                    repeated+=1
+                else:
+                    recent_text = text
+                if repeated>8 and recent_response!=text: # repeated for 4 seconds
+                    yield self.get_response(json.dumps({"text":text}))
+                    recent_response = text
+                    final_text += text + " "
+                    repeated = 0
+                    head = 40
+                    buffer = [buffer[0][:head] + buffer[-1][-8000+head:]]
+                elif partial:
+                    yield self.get_response(json.dumps({"partial":text}))
+        yield self.get_response(json.dumps({"final":final_text + text}))
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor((os.cpu_count() or 1)))
     stt_service_pb2_grpc.add_SttServiceServicer_to_server(
         SttServiceServicer(), server)
-    server.add_insecure_port('{}:{}'.format(vosk_interface, vosk_port))
+    server.add_insecure_port('{}:{}'.format("0.0.0.0", 5001))
     server.start()
+    print("started: localhost:5001")
     server.wait_for_termination()
-
 
 if __name__ == '__main__':
     logging.basicConfig()
