409c78270fa2865cb2263fa6c7ae3cf903f87509
Sani <khursani8@users.noreply.github.com>
2021-11-11 17:14:32 +0800

    Update sk.py
    add conformer medium

--- sk/sk.py
+++ sk/sk.py
@@ -92,7 +92,7 @@ def load_model(path):
     download_map = {
         "conformer_small": ["https://zenodo.org/record/5115792/files/conformer_small.onnx?download=1","https://f001.backblazeb2.com/file/suarakami/conformer_small.onnx"],
         "conformer_tiny": ["https://f001.backblazeb2.com/file/suarakami/conformer_tiny.onnx"],
-        "conformer_medium": ["https://f001.backblazeb2.com/file/suarakami/conformer_medium.onnx"]
+        "conformer_medium": ["https://zenodo.org/record/5674714/files/conformer_medium.onnx?download=1","https://f001.backblazeb2.com/file/suarakami/conformer_medium.onnx"]
     }
     if path.stem in cache_model:
         return cache_model[path.stem]
